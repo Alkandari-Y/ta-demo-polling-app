2cd785774fab6e4a77dd89bc0996f2634c1c9ad0
Alkandari-Y <yayalkandari@gmail.com>
2022-02-24 11:11:35 +0300

    edited data array

--- src/PollData.js
+++ src/PollData.js
@@ -1,22 +1,22 @@
 const PollData = [
   {
     id: 1,
-    title: "Test 1",
-    question: "Is this a test?",
+    title: "Soup",
+    question: "Do you enjoy Miso soup?",
     yesVotes: 20,
     noVotes: 7,
   },
   {
     id: 2,
-    title: "Test 2",
-    question: "Is this a test?",
+    title: "Travel",
+    question: "Are you a hiker?",
     yesVotes: 9,
     noVotes: 3,
   },
   {
     id: 3,
-    title: "Test 3",
-    question: "Is this a test?",
+    title: "2022",
+    question: "Are you fulfilling your new years resolutions?",
     yesVotes: 4,
     noVotes: 12,
   },
